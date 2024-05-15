20223dce1a1a978f0cd2f6a9829669d400880b27
Muli Job <munyoki912@gmail.com>
2024-05-15 18:37:48 +0300

    added routes to all template files

--- app/views.py
+++ app/views.py
@@ -1,11 +1,28 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
 
 # Views
 @app.route('/')
-def index():
+@app.route('/home')
+def home_page():
 
     '''
     View root page function that returns the index page and its data
     '''
     return render_template('base.html')
+
+@app.route('/signin')
+def sign_in():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def sign_up():
+    return render_template('signup.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/signout')
+def sign_out():
+    return redirect(url_for("index"))
