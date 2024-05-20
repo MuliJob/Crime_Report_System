ab8be555f16f449b42e4504ff1c84be866f52f20
Muli Job <munyoki912@gmail.com>
2024-05-20 16:53:58 +0300

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-# Initializing application
-
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'crime.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-app.config['SECRET_KEY'] = 'SECRET_KEY'
-
+app.config['SECRET_KEY'] = '\x08\xd9V7\x94\xf0\xce\xa47\n;N\x86\xcak\xbd\x96bP\xed&Z`\x8ep\x0cJ\xf9S\x1b\x1b\xf8\xfd\xd3-c\xc0f\xfe\xb6\x18\xder2\xef\x8f!\xb6N\r\xe6o\x182{\xc5a\xc2=\xd5c\x87n\xd3'
 
 
 from app import views
