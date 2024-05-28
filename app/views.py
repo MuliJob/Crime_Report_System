36d2d2b48e99bde0533db40907006e4362e035c8
Muli Job <munyoki912@gmail.com>
2024-05-28 10:13:03 +0300

    added login and registration of users

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request, flash
 from app import app, db
 from app.models import User, Register
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import login_user, login_required, logout_user
 
 
 # Views
@@ -29,13 +29,15 @@ def sign_in():
         
         if user:
             if check_password_hash(user.password, password):
-                flash(f'Logged in successfully! Welcome {username}', category='success')
+                flash(f'Logged in successfully!, Hello {username}', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('user_dashboard'))
             else: 
-                flash('Incorrect password, try again.', category='error')
+                flash('Incorrect password, try again.', category='danger')
         else:
-            flash('Username does not exist.', category='error')
+            flash('Username does not exist.', category='danger')
     
+    return render_template('signin.html')
     
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -49,15 +51,15 @@ def sign_up():
         user = Register.query.filter_by(username=username).first()
 
         if user:
-            flash('Username already exists.', category='error')
+            flash('Username already exists.', category='danger')
         elif len(username) < 2:
-            flash('Username must be more than 1 characters.', category='error')
+            flash('Username must be more than 1 characters.', category='danger')
         elif len(email) < 4:
-            flash('Email must be more than 4 characters.', category='error')
+            flash('Email must be more than 4 characters.', category='danger')
         elif password1 != password2:
-            flash('Password don\'t match.', category='error')
+            flash('Password don\'t match.', category='danger')
         elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+            flash('Password must be at least 7 characters.', category='danger')
         else:
             # add user to database
             new_user = Register(username=username, 
@@ -67,9 +69,11 @@ def sign_up():
                                 method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('register'))
-        
+    
+    return render_template('signup.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -81,11 +85,11 @@ def register():
         gender = request.form.get('gender')
 
         if len(idno) < 8:
-            flash('Identification Number must be equal to 8 characters.', category='error')
+            flash('Identification Number must be equal to 8 characters.', category='danger')
         elif len(fullname) < 4:
-            flash('Full Name must be more than 4 characters.', category='error')
+            flash('Full Name must be more than 4 characters.', category='danger')
         elif len(phonenumber) < 10:
-            flash('Phone Number must be equal to 10 characters', category='error')
+            flash('Phone Number must be equal to 10 characters', category='danger')
         else:
             personal_details = User(idno=idno, 
                                     fullname=fullname, 
@@ -94,17 +98,17 @@ def register():
                                     gender=gender)
             db.session.add(personal_details)
             db.session.commit()
-            flash(f"Welcome, {fullname}")
+            flash(f"Hello, {fullname}, welcome to the most secure website for reporting crimes", category='success')
             return redirect(url_for('user_dashboard'))
-    else:
-        flash('Failed to complete registration! Please try again later...', category='error')
-        return render_template('register.html')
+
+    return render_template('register.html')
 
 @app.route('/signout')
 @login_required
 def sign_out():
     logout_user()
-    return render_template('sign_in')
+    flash('You have been logged out.', category='info')
+    return redirect(url_for('sign_in'))
 
 @app.route('/dashboard')
 def user_dashboard():
