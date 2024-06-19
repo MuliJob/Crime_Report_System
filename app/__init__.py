bfbb5b5e47571400b6d38b7390a21fc6da1e8cd3
Muli Job <munyoki912@gmail.com>
2024-06-19 06:39:42 +0300

    completed flask migrate by adding constraints

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from app.config import NEWS_API, SECRET_KEY
@@ -8,6 +9,14 @@ from flask_migrate import Migrate
 
 api_key = NEWS_API
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 app = Flask(__name__, instance_relative_config=True)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] =\
@@ -15,7 +24,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
 app.config["SESSION_PERMANENT"]=False
 app.config['SESSION_TYPE']='filesystem'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db, render_as_batch=True)
 Session(app)
 
