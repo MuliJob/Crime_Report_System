1b1e57f89a4ac1cc07c63a1c1bbd68d51db8182c
Muli Job <munyoki912@gmail.com>
2024-07-25 12:19:28 +0300

    added column for phone number in officers table

--- app/officers/models.py
+++ app/officers/models.py
@@ -10,6 +10,7 @@ def current_time():
 class Officers(db.Model, UserMixin):
     officer_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    phonenumber = db.Column(db.String(20), nullable=True)
     officer_email = db.Column(db.String(120), nullable=False)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=True)
